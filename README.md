715b20d5a9e274e181a868bd412438290a39335d
cazongames <cazoncod7@gmail.com>
2025-05-05 01:36:41 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+# cazoncodltv.github.io
---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to toggle the menu
+function toggleMenu() {
+    const menu = document.querySelector('.nav-links');
+    menu.classList.toggle('active');
+}
+// transition effect pages
+window.addEventListener('load', function () {
+    document.body.classList.add('loaded');
+});
